b44e699e38a59d5ed1021f5d4586f5fdc3b25c8b
Vitalii Hribonkin <vitalikgribenkin@gmail.com>
2025-05-22 20:37:13 +0200

    Revert "Revert "add changes to App, UserAcountLayout, PrivateRoute and RestrictedRoute""

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,20 @@
-// PrivateRoute.jsx
-const PrivateRoute = ({ component: Component }) => {
-  return <Component />;
+// src/components/PrivateRoute.jsx
+
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from '../redux/auth/authSelectors';
+import Loader from './Loader/Loader';
+
+const PrivateRoute = ({ redirectTo = '/login' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  if (isRefreshing) return <Loader />;
+
+  return isLoggedIn ? <Outlet /> : <Navigate to={redirectTo} />;
 };
 
 export default PrivateRoute;
