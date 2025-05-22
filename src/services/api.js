22b3f628f9a9481ef6cb3daa1023e0978f1c765b
Vitalii Hribonkin <vitalikgribenkin@gmail.com>
2025-05-22 21:08:55 +0200

    change URL API

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://back-end-stackoverchill-793z.onrender.com",
 });
-
-// export const api = axios.create({
-//   baseURL: "https://back-end-stackoverchill-793z.onrender.com",
-// });
 
 export const setAuthHeader = (token) => {
   api.defaults.headers.common.Authorization = `Bearer ${token}`;
