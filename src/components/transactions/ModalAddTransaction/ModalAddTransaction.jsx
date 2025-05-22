e6c59e974478f56e4fca5abf5f70455cd5e2bdbc
Vitalii Hribonkin <vitalikgribenkin@gmail.com>
2025-05-22 20:44:05 +0200

    1

--- src/components/transactions/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/transactions/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,14 +1,33 @@
-import AddTransactionForm from "../AddTransactionForm/AddTransactionForm"
+import { useEffect } from "react";
+import AddTransactionForm from "../AddTransactionForm/AddTransactionForm";
+import s from "./ModalAddTransaction.module.css";
 
+const ModalAddTransaction = ({ onClose }) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") onClose();
+  };
 
-const ModalAddTransaction = () => {
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) onClose();
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
-    <>
-      
-      <AddTransactionForm/>
+    <div className={s.backdrop} onClick={handleBackdropClick}>
+      <div className={s.modal}>
+        <button className={s.close} onClick={onClose}>
+          <svg width="18" height="18">
+            <use href="/icons.svg#close"></use>
+          </svg>
+        </button>
+        <AddTransactionForm onCancel={onClose} />
+      </div>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default ModalAddTransaction
+export default ModalAddTransaction;
