de67194fa39d499c68766080cf67226a15fd6d19
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-05-25 23:42:47 +0300

    add changes in edit, add, and itemTransactions

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,10 +1,13 @@
+import { useSelector } from 'react-redux';
 import s from './Balance.module.css'
+import { selectUser } from '../../redux/user/userSelectors';
 
 const Balance = () => {
+  const user = useSelector(selectUser)
   return (
     <div className={s.balance}>
       <p className={s.title}>Your balance</p>
-      <h2 className={s.amount}>24 000.00 UAH</h2>
+      <h2 className={s.amount}>{user.balance} UAH</h2>
     </div>
   );
 }
